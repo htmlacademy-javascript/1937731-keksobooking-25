d1dc27878463f7511842ffa67ca874af82e8b34a
Keks <keks@htmlacademy.ru>
2022-02-24 20:59:21 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,5 +1,11 @@
 /* eslint-disable no-alert */
-function getRandomNumber(min, max, accuracy) {
+function getRandomInt (min, max) {
+  if (min<0 || min>=max || max<=0) {
+    alert('Неверные значения');
+  }
+  return Math.floor(Math.random()*max);
+}
+function getRandomFloat(min, max, accuracy) {
   if (min<0 || min>=max || max<=0) {
 
     alert('Неверные значения');
@@ -8,4 +14,5 @@ function getRandomNumber(min, max, accuracy) {
   return Math.floor((Math.random()*(max-min)+min)*afterDot)/afterDot;
 }
 
-getRandomNumber(0, 50, 3);
+getRandomFloat(0, 50, 3);
+getRandomInt(10, 25);
